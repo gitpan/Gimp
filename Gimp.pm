0de4de62bdfc61c5d339b9009ac89eb3bd1ce758
Marc Lehmann <schmorp@schmorp.de>
1998-07-02 20:00:37 +0000

    Import of MLEHMANN/Gimp-0.98 from CPAN.
    gitpan-cpan-distribution: Gimp
    gitpan-cpan-version:      0.98
    gitpan-cpan-path:         MLEHMANN/Gimp-0.98.tar.gz
    gitpan-cpan-author:       MLEHMANN
    gitpan-cpan-maturity:     released

--- Gimp.pm
+++ Gimp.pm
@@ -11,7 +11,7 @@ use vars qw($help $verbose $host);
 require DynaLoader;
 
 @ISA = qw(DynaLoader);
-$VERSION = '0.97';
+$VERSION = '0.98';
 
 @_param = qw(
 	PARAM_BOUNDARY	PARAM_CHANNEL	PARAM_COLOR	PARAM_DISPLAY	PARAM_DRAWABLE
