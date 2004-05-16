7afeffb6dd45077f4744390f30735b8634ee1e0f
Seth Burgess <sjburges@gimp.org>
2004-05-16 00:36:20 +0000

    Import of SJBURGES/Gimp-2.0 from CPAN.
    gitpan-cpan-distribution: Gimp
    gitpan-cpan-version:      2.0
    gitpan-cpan-path:         SJBURGES/Gimp2/Gimp-2.0.tar.gz
    gitpan-cpan-author:       SJBURGES
    gitpan-cpan-maturity:     released

--- Gimp.pm
+++ Gimp.pm
@@ -291,32 +291,6 @@ sub set_rgb_db($) {
    undef %rgb_db;
 }
 
-=head1 NAME
-
-canonicalize_colour - utility function for color conversion
-
-=head1 SYNOPSIS
-  
-  $color = canonicalize_colour ("#ff00bb");
-  $color = canonicalize_colour ([255,255,34]);
-  $color = canonicalize_colour ([255,255,34,255]);
-  $color = canonicalize_colour ([1.0,1.0,0.32]);
-  $color = canonicalize_colour ('red');
-
-=head1 DESCRIPTION
-
-Take in a color specifier in a variety of different formats, and return 
-a valid gimp color specifier, consisting of 3 or 4 numbers in the range 
-between 0 and 1.0.
-
-=head1 BUGS
- 
-  * bounds checking is excessively lax; assumes relatively good input
-  * should emit more warnings
-  * has an extra 'u' in 'color' :)
-
-=cut 
-
 sub canonicalize_colour {
    if ((scalar(@_) == 3) or (scalar(@_) == 4)) { # if 3 or 4 entries already
       if (($_[0] > 1.0) or ($_[1] > 1.0) or ($_[2] > 1.0)) {
@@ -712,11 +686,10 @@ package Gimp; # for __DATA__
 
 Gimp - Perl extension for writing Gimp Extensions/Plug-ins/Load & Save-Handlers
 
-WARNING: This is a pre-release of gimp-perl for gimp-2.0. If you are interested in a version for gimp-1.2, use version 1.2* of the Gimp-Perl module.
+This is a release of gimp-perl for gimp-2.0. If you are interested in a version for gimp-1.2, use version 1.2.x of the Gimp-Perl module instead.
 
 This is mostly a reference manual. For a quick intro, look at
-L<Gimp::Fu>. For more information, including tutorials, look at the
-Gimp-Perl pages at http://gimp.pages.de.
+L<Gimp::Fu>. 
 
 =head1 SYNOPSIS
 
@@ -768,11 +741,10 @@ The default (unless '' is specified) is C<'main', 'xlfd_size', ':consts', '__'>.
 
 =head1 GETTING STARTED
 
-You should first read the Gimp::Fu manpage and then come back. This manpage is mainly
-intended for reference purposes.
+You should first read the Gimp::Fu manpage and then come back. This manpage is mainly intended for reference purposes.
 
 Also, Dov Grobgeld has written an excellent tutorial for Gimp-Perl. You can
-find it at http://imagic.weizmann.ac.il/~dov/gimp/perl-tut.html
+find it at http://imagic.weizmann.ac.il/~dov/gimp/perl-tut-2.0/
 
 =head1 DESCRIPTION
 
@@ -1954,4 +1926,30 @@ __DATA__
 255 255	  0		yellow1
 238 238	  0		yellow2
 205 205	  0		yellow3
-139 139	  0		yellow4
+=head1 NAME
+
+canonicalize_colour - utility function for color conversion
+
+=head1 SYNOPSIS
+  
+  $color = canonicalize_colour ("#ff00bb");
+  $color = canonicalize_colour ([255,255,34]);
+  $color = canonicalize_colour ([255,255,34,255]);
+  $color = canonicalize_colour ([1.0,1.0,0.32]);
+  $color = canonicalize_colour ('red');
+
+=head1 DESCRIPTION
+
+Take in a color specifier in a variety of different formats, and return 
+a valid gimp color specifier, consisting of 3 or 4 numbers in the range 
+between 0 and 1.0.
+
+=head1 BUGS
+ 
+  * bounds checking is excessively lax; assumes relatively good input
+  * should emit more warnings
+  * has an extra 'u' in 'color' :)
+
+=cut 
+
+s139 139	  0		yellow4
